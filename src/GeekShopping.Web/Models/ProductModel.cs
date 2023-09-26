6585e352de1c613cabd68bab117824ccab0bcf4c
VanMachado <vandamme.machado@gmail.com>
2023-09-26 18:58:34 -0300

    Criacao dos services do GeekShopping.Web

---
+++ src/GeekShopping.Web/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class ProductModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public long CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
